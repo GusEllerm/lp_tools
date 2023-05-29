ea83bb3010474432f9d1e8368d8842621782c6a0
Augustus Ellerm <ellermaugustus@gmail.com>
2023-05-29 15:36:32 +1200

    update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='lp_tools',
+    name='lp_ap_tools',
     packages=find_packages(),
     version='0.1.0',
     description='A python decorator for creating ActionProvider RO-crates within a Globus flow',
@@ -11,5 +11,8 @@ setup(
     setup_requires=['pytest-runner'],
     test_requires=['pytest==4.4.1'],
     test_suite='tests',
-    url=
+    url='https://github.com/GusEllerm/lp_tools.git',
+    classifiers=("Programming Language :: Python :: 3",
+                 "License :: OSI Approved :: MIT License",
+                 "Operating System :: OS Independent")
 )
